d819dbe33a55f39f897e98e14e16e3c7b7aeaa52
Irina Draganova <irina.draganova21@gmail.com>
2026-03-16 13:49:56 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,109 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//Създаваме масив с цветовете
+// const colors = [
+
+//   '#FF6B6B',
+//   '#4D96FF',
+//   '#6BCB77',
+//   '#FFD93D',
+//   '#FF9F1C',
+//   '#845EC2',
+//   '#F72585',
+// ];
+
+//Създаваме масив с умения
+// const skills = [
+//   {
+//     name: 'HTML',
+//     emoji: '🧩',
+//   },
+
+//    {
+//     name: 'CSS',
+//     emoji: '🎨',
+//   },
+
+//    {
+//     name: 'JavaScript',
+//     emoji: '⚡',
+//   },
+
+//    {
+//     name: 'TypeScript',
+//     emoji: '🧠',
+//   },
+
+//    {
+//     name: 'Angular',
+//     emoji: '🅰️',
+//   },
+
+//    {
+//     name: 'React',
+//     emoji: '⚛️',
+//   },
+
+// ]
+
+function App() {
+  return (
+    //Това ни е рамката на картичката
+    <div className="card">
+      {/*React компоненти се извикват като HTML елементи, но започват с главна буква.*/}
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {}
+        <SkilList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img className="avatar" src="irina.png" alt="Irina Draganova" />;
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Irina Draganova</h1>
+      <p>
+        Junior Front-End Developer — a chaos of technologies in my head, but the
+        code still works ✔️
+      </p>
+    </div>
+  );
+}
+
+function SkilList(){
+  return (
+    <div className="skill-list">
+      <Skill skill="React" emoji="⚛️" color="#845EC2" />
+      <Skill skill="Angular" emoji="🅰️" color="#6BCB77" />
+      <Skill skill="HTML" emoji="🧩" color="#FF6B6B" />
+      <Skill skill="CSS" emoji="🎨" color="#F72585" />
+      <Skill skill="JavaScript" emoji="⚡" color="#4D96FF" />
+      <Skill skill="TypeScript" emoji="🧠" color="#FF9F1C" />
+    </div>
+  )
+}
+
+function Skill(props){
+  return(
+    <div className="skill" style={{backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
